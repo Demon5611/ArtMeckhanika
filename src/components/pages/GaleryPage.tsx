ff7bc0210501822f20c501a111c9b692c0d31456
Dmitry <d555601@gmail.com>
2023-11-16 10:27:11 +0300

    Update GaleryPage.tsx

--- src/components/pages/GaleryPage.tsx
+++ src/components/pages/GaleryPage.tsx
@@ -35,32 +35,30 @@ export default function GaleryPage(): JSX.Element {
   const [showModal, setShowModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  const openModal = (imageSrc: string) => {
+  const openModal = (imageSrc: string): void => {
     setSelectedImage(imageSrc);
     setShowModal(true);
   };
 
-  const closeModal = () => {
+  const closeModal = (): void => {
     setSelectedImage(null);
     setShowModal(false);
   };
-  const containerTextGalaryRight = {
-    // display: 'flex',
-    // flexWrap: 'wrap',
-    // justifyContent: 'flex-start',
-    // width: '48%',
-  };
+
   return (
     <>
-      <div className="containerTextGalaryTop">
-        <h2>Кто мы такие и куда направляемся?</h2>
+      <div className="containerTextGalaryTop" >
+        <h3 className='h3_textTop'>Кто мы такие и куда направляемся?</h3>
         <br />
+        <p style={{ textIndent: '2ch' }}>
         Этот вопрос не раз ставил меня в тупик, потому что я родитель, и в то же время, обладаю
         любопытством ребенка, выросшего в семье ученых. Скажем по-другому. Для меня это второй
-        вопрос после "Есть ли Господь?".
-        <div className="containerTextGalaryRightALL" style={containerTextGalaryRight}>
+        вопрос после "Есть ли Господь?". Я думаю, что ответ уже ясен из заглавной буквы слова
+        "Господь". Но как насчет остального?
+        </p>
+        <div className="containerTextGalaryRightALL" >
           <div className="textRight1">
-            <h2 style={{ marginTop: '5ch' }}>ПРОЦЕСС ЭВОЛЮЦИИ</h2>
+            <h3 className='h1_1' style={{ marginTop: '1ch' }}>ПРОЦЕСС ЭВОЛЮЦИИ</h3>
             <p style={{ textIndent: '2ch' }}>
               Это яркий вызов современной реальности Нет ничего хуже интеллектуальных шор навязанных
               нам….Кем навязанных? Социальными предрассудками, бизнесом и личными интересами
@@ -70,15 +68,15 @@ export default function GaleryPage(): JSX.Element {
             <p className="material">Материал: смешанная техника, графика. Размеры работ 80/100см</p>
           </div>
 
-          <div className="textRight1">
-            <h2 style={{ marginTop: '4ch' }}>КОРОЛЕВ</h2>
+          <div className="textRight2">
+            <h3 className='h2_2'>КОРОЛЕВ</h3>
             <p style={{ textIndent: '2ch' }}>Размышление на тему живопись и любимый город</p>
-            <br />
+            {/* <br /> */}
             <p className="material">Материал: масло, пастель; графика. Размеры работ 30/40см</p>
           </div>
 
-          <div className="textRight1">
-            <h2 style={{ marginTop: '7ch', textIndent: '0ch' }}>МИГРАЦИОННЫЙ ЗАХВАТ</h2>
+          <div className="textRight3">
+            <h3 className='h3_3' >МИГРАЦИОННЫЙ ЗАХВАТ</h3>
             <p style={{ textIndent: '2ch' }}>
               Эта проблема больше чем вам всем кажется… Слишком много людей мигрируют по странам и
               континентам – вы все еще сомневаетесь? Посмотрите статистику. Смешение культур, расс,
@@ -89,28 +87,28 @@ export default function GaleryPage(): JSX.Element {
             </p>
             <a
               href={NataAvodesCV}
-              style={{ float: 'right' }}
+              style={{ float: 'right', fontSize: '1ch' }}
               className="cv"
               download="NataAvodesCV.pdf"
             >
-              Скачать NataAvodesCV.pdf
+              Скачать CV NataAvodes
             </a>
           </div>
         </div>
-        <div
-          className="image-container"
-          style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', width: '50%' }}
-        >
+
+
+        <div  className="image-container">
           {imagesData.map((image, index) => (
-            <Card
+            <Card 
               key={image.id || index}
               onClick={() => openModal(image.src)}
-              style={{ margin: '45px' }}
-            >
-              <Card.Img variant="top" src={image.src} />
+              >
+              <Card.Img  src={image.src} />
             </Card>
           ))}
         </div>
+
+            
       </div>
 
       <Modal show={showModal} onHide={closeModal} size="lg" centered>
