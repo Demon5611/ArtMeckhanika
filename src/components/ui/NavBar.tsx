633139f4317918b3598d6cb1f6e7cef3f0a22d6f
Dmitry <d555601@gmail.com>
2023-11-21 22:31:29 +0300

    Update NavBar.tsx

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -7,7 +7,7 @@ import logoNata from '../../assets/img/logoNataAvodes.png';
 export default function NavBar(): JSX.Element {
   useEffect(() => {
     const navBar = document.querySelector('.nuvBar');
-
+    console.log('Scrolling...', window.scrollY);
     if (navBar) {
       const scrollHandler = (): void => {
         if (window.scrollY > 0) {
@@ -25,17 +25,23 @@ export default function NavBar(): JSX.Element {
     }
   }, []);
 
+
   return (
     <Container>
       <Navbar className="nuvBar" variant="light">
         <img src={logoNata} width="30%" height="30%" className="me-auto me-9" alt="logo" />
         <Nav className="ms-4">
-          <Nav.Link href="/">Главная</Nav.Link>
-          <Nav.Link href="/gallery">Галерея</Nav.Link>
-          <Nav.Link href="/contacts">Контакты</Nav.Link>
+          <Nav.Link href="#main">
+            Главная
+          </Nav.Link>
+          <Nav.Link href="#gallery">
+            Галерея
+          </Nav.Link>
+          <Nav.Link href="#contacts">
+            Контакты
+          </Nav.Link>
         </Nav>
       </Navbar>
     </Container>
   );
 }
-
