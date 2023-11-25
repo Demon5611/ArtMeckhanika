2f38eb0961b1a008a297f3a566457df1b043fcb0
Dmitry <d555601@gmail.com>
2023-11-25 18:56:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import ContactsPage from './components/pages/ContactsPage';
-import GaleryPage from './components/pages/GaleryPage';
 import MainPage from './components/pages/MainPage';
 import NavBar from './components/ui/NavBar';
+import Footer from './components/ui/Footer';
 
 function App(): JSX.Element {
   return (
     <>
       <NavBar />
+
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/gallery" element={<GaleryPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
       </Routes>
+
+      <Footer />
     </>
   );
 }
