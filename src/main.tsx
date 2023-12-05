9a5dd402d99be8aefc62ae16e1628a079181692f
Dmitry <d555601@gmail.com>
2023-12-05 15:11:28 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,17 @@
-import  React  from 'react';
 import { Container } from 'react-bootstrap';
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ScrollToTopButton from './components/ui/ScrollToTopButton';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-<BrowserRouter>
-  
-      <Container className="main-container">
-        <App />
-        <ScrollToTopButton />
-      </Container>
-  
- </BrowserRouter>,
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Container className="main-container">
+      <App />
+      <ScrollToTopButton />
+    </Container>
+  </React.StrictMode>,
 );
